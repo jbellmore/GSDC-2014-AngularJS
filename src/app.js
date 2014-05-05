e921bad7e8d7e63be0647afd65b85370e912c835
Jarrod Bellmore <jbellmore@gmail.com>
2014-05-05 15:39:24 -0400

    Added sessions to home page

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 var app = angular.module('myApp', ['ui.router']);
 
-app.run(function () {
+app.run(function ($rootScope) {
     alert('myApp has started');
+
+    $rootScope.getBaseUrl = function () {
+      // return "http://localhost:1998/api/v1/";
+      return "http://glic.azurewebsites.net/api/v1/";
+    };
 });
--- src/home/home-controller.js
+++ src/home/home-controller.js
@@ -7,8 +7,19 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('homeController', function () {
+app.controller('homeController', function ($http, $rootScope, $scope) {
     var self = this;
 
-    alert('Home controller loaded');
+    self.loadSessions = function () {
+        var url = $rootScope.getBaseUrl() + "conference/1/session";
+        $http({ method: 'get', url: url })
+          .success(function (data, status, headers, config) {
+              $scope.sessions = data;
+          })
+          .error(function (data, status, headers, config) {
+              $scope.errorMessage = "An error occured while attempting to register.";
+          });
+    };
+
+    self.loadSessions();
 });
